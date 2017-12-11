53a74c7d6ea5f2e6de6a351224e6feac68f3e048
Eugene Kortov <kortovea@gmail.com>
2017-12-11 06:29:38 +0400

    #35: implemented getTableNames method
    - added corresponding method
    - added schema field to DBmanager
    - added tests

--- src/main/java/juja/sqlcmd/DatabaseManager.java
+++ src/main/java/juja/sqlcmd/DatabaseManager.java
@@ -2,12 +2,25 @@ package juja.sqlcmd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DatabaseManager {
     private final String jdbcDriverClass;
     private final String jdbcUrl;
+    private String schemaPattern = "public";
     private Connection connection;
+
+    public String getSchemaPattern() {
+        return schemaPattern;
+    }
+
+    public void setSchemaPattern(String schemaPattern) {
+        this.schemaPattern = schemaPattern;
+    }
 
     public DatabaseManager() {
         jdbcDriverClass = "org.postgresql.Driver";
@@ -30,4 +43,21 @@ public class DatabaseManager {
             return false;
         }
     }
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseManager manager = new DatabaseManager();
+        manager.connect("sqlcmd", "sqlcmd", "sqlcmd");
+        System.out.println(Arrays.toString(manager.getTableNames()));
+    }
+
+    public String[] getTableNames() throws SQLException {
+        try (ResultSet rs = connection.getMetaData().getTables(null, schemaPattern, "%", new String[]{"TABLE"})) {
+            List<String> tablesList = new LinkedList<>();
+            while (rs.next()) {
+                tablesList.add(rs.getString("table_name"));
+            }
+            return tablesList.toArray(new String[0]);
+        }
+    }
+
 }
--- src/test/java/juja/sqlcmd/DatabaseManagerTest.java
+++ src/test/java/juja/sqlcmd/DatabaseManagerTest.java
@@ -1,29 +1,59 @@
 package juja.sqlcmd;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class DatabaseManagerTest {
+    private static final String SCHEMA_PATTERN = "test";
+    private static final String DB_NAME = "sqlcmd";
+    private static final String DB_USER = "sqlcmd";
+    private static final String DB_USER_PASSWORD = "sqlcmd";
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/";
+    private static DatabaseManager databaseManager;
+    private static Connection connection;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private DatabaseManager databaseManager;
     private PrintStream originalOut;
     private PrintStream originalErr;
 
+    @BeforeClass
+    public static void setTestingEnvironment() throws SQLException {
+        databaseManager = new DatabaseManager();
+        connection = DriverManager.getConnection(
+                JDBC_URL + DB_NAME + "?currentschema" + SCHEMA_PATTERN, DB_USER, DB_USER_PASSWORD);
+        recreateDbSchema();
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
     @Before
     public void setUpStreamsAndDbManager() {
+        databaseManager = new DatabaseManager();
+        databaseManager.setSchemaPattern(SCHEMA_PATTERN);
+        databaseManager.connect(DB_NAME, DB_USER, DB_USER_PASSWORD);
+
         originalOut = System.out;
         originalErr = System.err;
         System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
-        databaseManager = new DatabaseManager();
+
     }
 
     @After
@@ -33,34 +63,62 @@ public class DatabaseManagerTest {
     }
 
     @Test
-    public void testConnectWithAllValidParameters() {
-        assertTrue(databaseManager.connect("sqlcmd", "sqlcmd", "sqlcmd"));
+    public void ConnectWithAllValidParameters() {
+        assertTrue(databaseManager.connect(DB_NAME, DB_USER, DB_USER_PASSWORD));
     }
 
     @Test
-    public void testConnectWithInvalidPassword() {
-        assertFalse(databaseManager.connect("sqlcmd", "sqlcmd", "wrongPass"));
+    public void ConnectWithInvalidPassword() {
+        assertFalse(databaseManager.connect(DB_NAME, DB_USER, "wrongPass"));
     }
 
     @Test
-    public void testConnectWithInvalidUserName() {
-        assertFalse(databaseManager.connect("sqlcmd", "noSqlcmd", "sqlcmd"));
+    public void ConnectWithInvalidUserName() {
+        assertFalse(databaseManager.connect(DB_NAME, "noSqlcmd", DB_USER_PASSWORD));
     }
 
     @Test
-    public void testConnectWithInvalidDbName() {
-        assertFalse(databaseManager.connect("doesNotExist", "sqlcmd", "sqlcmd"));
+    public void ConnectWithInvalidDbName() {
+        assertFalse(databaseManager.connect("doesNotExist", DB_USER, DB_USER_PASSWORD));
     }
 
     @Test
-    public void testInitDatabaseManagerWithInvalidJdbcDriver() {
+    public void InitDatabaseManagerWithInvalidJdbcDriver() {
         databaseManager = new DatabaseManager("noDriver", "jdbc:postgresql://localhost:5432/");
-        assertFalse(databaseManager.connect("sqlcmd", "sqlcmd", "sqlcmd"));
+        assertFalse(databaseManager.connect(DB_NAME, DB_USER, DB_USER_PASSWORD));
     }
 
     @Test
-    public void testInitDatabaseManagerWithInvalidJdbcUrl() {
+    public void InitDatabaseManagerWithInvalidJdbcUrl() {
         databaseManager = new DatabaseManager("org.postgresql.Driver", "noJdbcUrl");
-        assertFalse(databaseManager.connect("sqlcmd", "sqlcmd", "sqlcmd"));
+        assertFalse(databaseManager.connect(DB_NAME, DB_USER, DB_USER_PASSWORD));
+    }
+
+    @Test
+    public void getTableNamesWith2tables() throws SQLException {
+        executeSqlQuery("CREATE TABLE " + SCHEMA_PATTERN + ".table1()");
+        executeSqlQuery("CREATE TABLE " + SCHEMA_PATTERN + ".table2()");
+        String[] expectedArray = {"table1", "table2"};
+        String[] actualArray = databaseManager.getTableNames();
+        assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void getTableNamesReturnsArrayWith2Tables() throws SQLException {
+        recreateDbSchema();
+        String[] expectedArray = new String[]{};
+        String[] actualArray = databaseManager.getTableNames();
+        assertArrayEquals(expectedArray, actualArray);
+    }
+
+    private static void executeSqlQuery(String sqlQuery) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sqlQuery);
+        }
+    }
+
+    private static void recreateDbSchema() throws SQLException {
+        executeSqlQuery("DROP SCHEMA IF EXISTS " + SCHEMA_PATTERN + " CASCADE");
+        executeSqlQuery("CREATE SCHEMA " + SCHEMA_PATTERN);
     }
 }
