6e902ce947dc2f557041995bd6755fe7194c2a19
Eugene Kortov <kortovea@gmail.com>
2018-01-26 07:11:33 +0400

    #95 Separated users for db in tests, segregated test for connect - Separated logic for db's admin functional (set testing environment) and ordinary user for working with db - Segregated test for connecting to the db

--- src/test/java/juja/sqlcmd/DatabaseManagerTest.java
+++ src/test/java/juja/sqlcmd/DatabaseManagerTest.java
@@ -6,8 +6,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,88 +13,47 @@ import java.sql.Statement;
 
 import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class DatabaseManagerTest {
-    private static final String TEST_DB_NAME = "test";
-    private static final String DB_USER_NAME = "sqlcmd";
+    private static final String ADMIN_DB_NAME = "postgres";
+    private static final String TEST_DB_NAME = "sqlcmd_test";
+    private static final String DB_ADMIN_LOGIN = "postgres";
+    private static final String DB_ADMIN_PASSWORD = "postgres";
+    private static final String DB_USER_LOGIN = "sqlcmd";
     private static final String DB_USER_PASSWORD = "sqlcmd";
     private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/";
-    private static final String TEST_TABLE_NAME = "test_table";
     private static Connection connection;
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private PrintStream originalOut;
-    private PrintStream originalErr;
     private DatabaseManager databaseManager;
 
     @BeforeClass
     public static void setTestingEnvironment() throws SQLException {
-        connection = DriverManager.getConnection(
-                JDBC_URL, DB_USER_NAME, DB_USER_PASSWORD);
+        connection = DriverManager.getConnection(JDBC_URL + ADMIN_DB_NAME, DB_ADMIN_LOGIN, DB_ADMIN_PASSWORD);
         executeSqlQuery("DROP DATABASE IF EXISTS " + TEST_DB_NAME);
-        executeSqlQuery("CREATE DATABASE " + TEST_DB_NAME);
+        executeSqlQuery("CREATE DATABASE " + TEST_DB_NAME + " OWNER =" + DB_USER_LOGIN);
         connection.close();
-        connection = DriverManager.getConnection(
-                JDBC_URL + TEST_DB_NAME, DB_USER_NAME, DB_USER_PASSWORD);
+        connection = DriverManager.getConnection(JDBC_URL + TEST_DB_NAME, DB_ADMIN_LOGIN, DB_ADMIN_PASSWORD);
+        executeSqlQuery("ALTER SCHEMA public OWNER TO " + DB_USER_LOGIN);
+        connection.close();
+        connection = DriverManager.getConnection(JDBC_URL + TEST_DB_NAME, DB_USER_LOGIN, DB_USER_PASSWORD);
     }
 
     @Before
-    public void setUpStreamsAndDbManager() throws SQLException {
+    public void setUpDbManager() throws SQLException {
         recreateDbSchema();
         databaseManager = new DatabaseManager();
-        databaseManager.connect(TEST_DB_NAME, DB_USER_NAME, DB_USER_PASSWORD);
-        originalOut = System.out;
-        originalErr = System.err;
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
+        databaseManager.connect(TEST_DB_NAME, DB_USER_LOGIN, DB_USER_PASSWORD);
     }
 
     @After
-    public void cleanUpStreams() throws SQLException {
+    public void closeDbManagerConnection() throws SQLException {
         databaseManager.close();
-        System.setOut(originalOut);
-        System.setErr(originalErr);
     }
 
     @AfterClass
     public static void closeConnection() throws SQLException {
         connection.close();
-    }
-
-    @Test
-    public void connectWithAllValidParameters() {
-        assertTrue(databaseManager.connect(TEST_DB_NAME, DB_USER_NAME, DB_USER_PASSWORD));
-    }
-
-    @Test
-    public void connectWithInvalidPassword() {
-        assertFalse(databaseManager.connect(TEST_DB_NAME, DB_USER_NAME, "wrongPass"));
-    }
-
-    @Test
-    public void connectWithInvalidUserName() {
-        assertFalse(databaseManager.connect(TEST_DB_NAME, "noSqlcmd", DB_USER_PASSWORD));
-    }
-
-    @Test
-    public void connectWithInvalidDbName() {
-        assertFalse(databaseManager.connect("doesNotExist", DB_USER_NAME, DB_USER_PASSWORD));
-    }
-
-    @Test
-    public void initDatabaseManagerWithInvalidJdbcDriver() {
-        databaseManager = new DatabaseManager("noDriver", "jdbc:postgresql://localhost:5432/");
-        assertFalse(databaseManager.connect(TEST_DB_NAME, DB_USER_NAME, DB_USER_PASSWORD));
-    }
-
-    @Test
-    public void initDatabaseManagerWithInvalidJdbcUrl() {
-        databaseManager = new DatabaseManager("org.postgresql.Driver", "noJdbcUrl");
-        assertFalse(databaseManager.connect(TEST_DB_NAME, DB_USER_NAME, DB_USER_PASSWORD));
     }
 
     @Test
@@ -117,9 +74,9 @@ public class DatabaseManagerTest {
 
     @Test
     public void getTableDataWithEmptyTable() throws SQLException {
-        executeSqlQuery("CREATE TABLE " + TEST_TABLE_NAME + "()");
+        executeSqlQuery("CREATE TABLE test_table()");
         DataSet[] expectedArray = new DataSet[0];
-        assertArrayEquals(expectedArray, databaseManager.getTableData(TEST_TABLE_NAME));
+        assertArrayEquals(expectedArray, databaseManager.getTableData("test_table"));
     }
 
     @Test
@@ -130,9 +87,12 @@ public class DatabaseManagerTest {
 
     @Test
     public void getTableDataWithValidTableTwoRows() throws SQLException {
-        createTestTableWithIdAndName(TEST_TABLE_NAME);
-        executeSqlQuery("INSERT INTO " + TEST_TABLE_NAME + " VALUES(1,'name1')");
-        executeSqlQuery("INSERT INTO " + TEST_TABLE_NAME + " VALUES(2,'name2')");
+        executeSqlQuery("CREATE TABLE test_table(" +
+                "id INTEGER," +
+                "name VARCHAR(128)" +
+                ")");
+        executeSqlQuery("INSERT INTO test_table VALUES(1,'name1')");
+        executeSqlQuery("INSERT INTO test_table VALUES(2,'name2')");
         DataSet row1 = new DataSet(2);
         row1.insertValue(0, "1");
         row1.insertValue(1, "name1");
@@ -140,48 +100,8 @@ public class DatabaseManagerTest {
         row2.insertValue(0, "2");
         row2.insertValue(1, "name2");
         DataSet[] expectedArray = new DataSet[]{row1, row2};
-        DataSet[] actualArray = databaseManager.getTableData(TEST_TABLE_NAME);
+        DataSet[] actualArray = databaseManager.getTableData("test_table");
         assertThat(actualArray, arrayContainingInAnyOrder(expectedArray));
-    }
-
-    @Test
-    public void insertWithExistingTable() throws SQLException {
-        createTestTableWithIdAndName(TEST_TABLE_NAME);
-        DataSet row = new DataSet(2);
-        row.insertValue(0, "1");
-        row.insertValue(1, "name1");
-        assertTrue(databaseManager.insert(TEST_TABLE_NAME, row));
-    }
-
-    @Test
-    public void insertWithNotExistingTable() {
-        DataSet row = new DataSet(2);
-        row.insertValue(0, "1");
-        row.insertValue(1, "name1");
-        assertFalse(databaseManager.insert("tableDoesNotExist", row));
-    }
-
-    @Test
-    public void insertWithExtraParameters() throws SQLException {
-        createTestTableWithIdAndName(TEST_TABLE_NAME);
-        DataSet row = new DataSet(3);
-        row.insertValue(0, "1");
-        row.insertValue(1, "name1");
-        row.insertValue(2, "name2");
-        assertFalse(databaseManager.insert(TEST_TABLE_NAME, row));
-    }
-
-    @Test
-    public void insertIntoTableRowWithoutId() throws SQLException {
-        createTestTableWithIdAndName(TEST_TABLE_NAME);
-        DataSet row = new DataSet(2);
-        row.insertValue(1, "name1");
-        assertFalse(databaseManager.insert(TEST_TABLE_NAME, row));
-    }
-
-    private void createTestTableWithIdAndName(String tableName) throws SQLException {
-        executeSqlQuery("CREATE TABLE " + tableName +
-                "(id INTEGER, name VARCHAR(128))");
     }
 
     private static void executeSqlQuery(String sqlQuery) throws SQLException {
@@ -191,7 +111,7 @@ public class DatabaseManagerTest {
     }
 
     private static void recreateDbSchema() throws SQLException {
-        executeSqlQuery("DROP SCHEMA public CASCADE");
-        executeSqlQuery("CREATE SCHEMA public");
+        executeSqlQuery("DROP SCHEMA IF EXISTS public CASCADE");
+        executeSqlQuery("CREATE SCHEMA public AUTHORIZATION " + DB_USER_LOGIN);
     }
 }
