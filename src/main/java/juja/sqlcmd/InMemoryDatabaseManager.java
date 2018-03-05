af20ee7c03726755a9c45d54593715a49fb26745
Eugene Kortov <kortovea@gmail.com>
2018-03-06 00:22:50 +0400

    #109 added inMemoryDatabaseManager class with tests

---
+++ src/main/java/juja/sqlcmd/InMemoryDatabaseManager.java
@@ -0,0 +1,95 @@
+package juja.sqlcmd;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryDatabaseManager implements DatabaseManager {
+    private Map<String, Table> tables;
+
+    public InMemoryDatabaseManager() {
+        this.tables = new HashMap<>();
+    }
+
+    public void createTable(String tableName, int columns_number) {
+        if (tableName == null) {
+            throw new IllegalArgumentException("Table name must not be null!");
+        }
+        if (columns_number < 0) {
+            throw new IllegalArgumentException("Number of columns must be positive!");
+        }
+        tables.put(tableName, new Table(columns_number));
+    }
+
+    @Override
+    public boolean connect(String database, String user, String password) {
+        return true;
+    }
+
+    @Override
+    public void close() {
+        //No operation
+    }
+
+    @Override
+    public String[] getTableNames() {
+        return tables.keySet().toArray(new String[0]);
+    }
+
+    @Override
+    public DataSet[] getTableData(String tableName) {
+        Table table = tables.get(tableName);
+        return table != null ? table.tableData() : new DataSet[0];
+    }
+
+    @Override
+    public boolean insert(String tableName, DataSet dataset) {
+        Table table = tables.get(tableName);
+        return table != null && table.insert(dataset);
+    }
+
+    @Override
+    public boolean delete(String tableName, int id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean update(String tableName, int id) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static class Table {
+        private List<DataSet> rows;
+        private int columnsNumber;
+
+        Table(int columnsNumber) {
+            this.columnsNumber = columnsNumber;
+            rows = new ArrayList<>();
+        }
+
+        DataSet[] tableData() {
+            return rows.toArray(new DataSet[0]);
+        }
+
+        boolean insert(DataSet row) {
+            if (row.getRowSize() != columnsNumber) {
+                return false;
+            }
+            if (isRowContainsNull(row)) {
+                return false;
+            }
+            rows.add(row);
+            return true;
+        }
+
+        private boolean isRowContainsNull(DataSet row) {
+            for (String s : row.values()) {
+                if (s == null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/test/java/juja/sqlcmd/InMemoryDatabaseManagerTest.java
@@ -0,0 +1,32 @@
+package juja.sqlcmd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class InMemoryDatabaseManagerTest extends AbstractDatabaseManagerTest {
+    private InMemoryDatabaseManager inMemoryDatabaseManager;
+
+    @Before
+    public void setUp() {
+        super.init(new InMemoryDatabaseManager());
+    }
+
+    @Test
+    public void connect() {
+        assertTrue(databaseManager.connect(null, null, null));
+    }
+
+    @Override
+    void createTestTableWithIdAndName(String tableName) {
+        inMemoryDatabaseManager = (InMemoryDatabaseManager) databaseManager;
+        inMemoryDatabaseManager.createTable(tableName, 2);
+    }
+
+    @Override
+    void insertData(String tableName, DataSet row) {
+        inMemoryDatabaseManager = (InMemoryDatabaseManager) databaseManager;
+        inMemoryDatabaseManager.insert(tableName, row);
+    }
+}
